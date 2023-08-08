cff4a73d9cf3ef2dde9f5d7eca731d8ee778484a
abhijeet <abhijeet.k@binmile.com>
2023-08-08 11:19:19 +0530

    added variables

---
+++ variable.js
@@ -0,0 +1,15 @@
+/*Variables defined with let cannot be Redeclared
+
+Variables defined with let must be Declared before use
+
+Variables defined with let have Block Scope */ 
+
+let x = 10;
+
+/*Variables defined with const cannot be Redeclared
+
+Variables defined with const cannot be Reassigned
+
+Variables defined with const have Block Scope*/
+
+const y = "your name is";
