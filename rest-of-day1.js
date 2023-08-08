6d2d6d59297b7a373b175eac771feab80f0754d2
abhijeet <abhijeet.k@binmile.com>
2023-08-08 12:20:18 +0530

    rest of day-1 added

---
+++ rest-of-day1.js
@@ -0,0 +1,36 @@
+// Numbers:
+let height = 16;
+let weight = 7.5;
+
+// Strings:
+let firstName = "rock";
+let lastName = "Johnson";
+
+// Booleans
+let a = true;
+let b = false;
+
+// Object:
+const person = {class:"11", subject:"science"};
+
+// Array object:
+const cars = ["maruti", "hundai", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-28");
+
+// Methods of primitives
+let str = "Welcome to BINMILE";
+console.log(str.toUpperCase()); 
+
+
+// Array Methods
+// push pop shift unshift
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+
+
+// Iterables
+for (const c of [1,2,3,4,5]) {
+    console.log(c)
+  }
