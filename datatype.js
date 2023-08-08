bddf3874f4b52407f32739858b684bfb8fc844dc
abhijeet <abhijeet.k@binmile.com>
2023-08-08 11:59:10 +0530

    added datatypes

---
+++ datatype.js
@@ -0,0 +1,47 @@
+
+
+//JavaScript has 8 Datatypes
+
+//Number
+
+let num1 = 32;
+
+//BigInt
+
+const bigint = 8562323159475639012345678901234567890n;
+
+//String
+
+let str = "Hello";
+let str2 = 'Welcome to W3Docs!';
+
+//Boolean
+
+let x1 = true;
+let x2 = false;
+
+//Null
+
+let price = null;
+
+//Undefined
+
+let x;
+
+//Symbol
+
+let symbol1 = Symbol("symbol")
+
+
+// the above are primitve data-types   
+// objects are not primitive data-types
+
+Object
+
+let obj = {
+    key1: 'value1',
+    key2: 'value2',
+    key3: true,
+    key4: 20,
+    key5: {}
+  }
